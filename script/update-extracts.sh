cc2aca6bc219359ccff9d0c53a3a2bf097d8c9c9
mvexel <m@rtijn.org>
2014-09-18 13:45:11 -0600

    renaming POLY files

--- script/update-extracts.sh
+++ script/update-extracts.sh
@@ -7,24 +7,24 @@ cd $BASEDIR/bin
 # make the current planet old
 echo 'going to back up old planet'
 date
-mv $BASEDIR/planet/planet.o5m $BASEDIR/planet/planet_old.o5m
+mv $BASEDIR/planet/planet.osm.pbf $BASEDIR/planet/planet_old.osm.pbf
 # update the planet
 echo 'going to update planet'
 date
-./osmupdate  -t=$BASEDIR/tmp/osmupdate $BASEDIR/planet/planet_old.o5m $BASEDIR/planet/planet.o5m
-# create US extract
-echo 'going to create US extract'
+./osmupdate  -t=$BASEDIR/tmp/osmupdate $BASEDIR/planet/planet_old.osm.pbf $BASEDIR/planet/planet.osm.pbf
+# splitting
+echo 'going to split into US and States (current)'
 date
-./osmconvert -B=$BASEDIR/poly/us.poly /mnt/osm/planet/planet.o5m -o=$BASEDIR/planet/us.o5m
+$BASEDIR/script/split-current.py
 # convert to pbf
-echo 'going to convert US extract to PBF'
-date
-./osmconvert $BASEDIR/planet/us.o5m -o=$BASEDIR/tmp/pbf/us.osm.pbf
+#echo 'going to convert US extract to PBF'
+#date
+#./osmconvert $BASEDIR/planet/us.o5m -o=$BASEDIR/tmp/pbf/us.osm.pbf
 # split into states
-echo 'going to split US extract to states'
-date
-mkdir -p $BASEDIR/tmp/pbf/states
-for c in /mnt/osm/conf/*.conf;do $BASEDIR/bin/osm-history-splitter --hardcut $BASEDIR/tmp/pbf/us.osm.pbf $c;done
+#echo 'going to split US extract to states'
+#date
+#mkdir -p $BASEDIR/tmp/pbf/states
+#for c in /mnt/osm/conf/*.conf;do $BASEDIR/bin/osm-history-splitter --hardcut $BASEDIR/tmp/pbf/us.osm.pbf $c;done
 # copy
 echo 'moving everything in place'
 date
