3c28c7836795f89d2a92b033ef0c40b2a262edd0
mvexel <m@rtijn.org>
2014-12-09 12:53:44 -0700

    adding highway extract script

---
+++ script/extract-highways.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+shopt -s nullglob
+
+FILES=$1/*.pbf
+OUTDIR=$2
+
+for f in $FILES
+    do
+        echo "Processing $f..."
+        FILENAME=$(basename "$f")
+        FILENAME=${FILENAME%%.*}
+        OUTFILE="$OUTDIR/$FILENAME-highways.osm.pbf"
+        echo $OUTFILE
+        /mnt/osm/bin/osmosis --rb $f --tf accept-ways highway=* --tf reject-relations --used-node --wb $OUTFILE
+    done
