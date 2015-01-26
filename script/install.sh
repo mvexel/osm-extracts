3c277518ad4c29cfa4b5f298edcdba115b3a0ce5
Martijn van Exel <m@rtijn.org>
2015-01-26 10:17:44 +0100

    changing install script to use submodules

--- script/install.sh
+++ script/install.sh
@@ -1,23 +1,27 @@
+#!/bin/bash
+
+# base dir from arg
+let BASE_DIR=$1
+let EXTRACTS_DIR=$BASEDIR/extracts
+
+# Clone OSM Extracts repo
+if [ ! -d $EXTRACTS_DIR ]; then
+    git clone https://github.com/mvexel/osm-extracts.git $EXTRACTS_DIR
+fi
+
 # Install osmium requirements
 sudo aptitude install zlib1g-dev libexpat1-dev libxml2-dev libgeos-dev libgeos++-dev libsparsehash-dev libprotobuf-dev protobuf-compiler libosmpbf-dev
-# Clone OSM Extracts repo
-if [ ! -d $[myJob/basedir] ]; then
-    git clone https://github.com/mvexel/osm-extracts.git $[myJob/basedir]
-fi
+
 # Get Osmium
-cd $[myJob/basedir]/lib
-git clone https://github.com/joto/osmium.git
-cd osmium
+cd $EXTRACTS_DIR/external/osmium
 sudo make install
+
 # Get osm-history-splitter
-cd $[myJob/basedir]/bin
-git clone https://github.com/MaZderMind/osm-history-splitter.git
-cd osm-history-splitter
+cd $EXTRACTS_DIR/external/osm-history-splitter
 make
-cd $[myJob/basedir]/bin
+ln -s $EXTRACTS_DIR/lib/osm-history-splitter/osm-history-splitter $EXTRACTS_DIR/bin/osm-history-splitter
+
+# Get osmconvert and osmupdate
+cd EXTRACTS_DIR/bin
 hash osmconvert 2>/dev/null || wget -O - http://m.m.i24.cc/osmconvert.c | cc -x c - -lz -O3 -o osmconvert
 hash osmupdate 2>/dev/null || wget -O - http://m.m.i24.cc/osmupdate.c | cc -x c - -o osmupdate
-
-cd osmium
-sudo make install
-cd ..
